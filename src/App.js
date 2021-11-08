eb210ef76e945a4fc63bc49ff3708478245dcdc8
Henry Cortez <Henry200amaya@gmail.com>
2021-11-07 20:37:06 -0600

    :sparkles: Add pokemon services

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import './App.css';
-
-import Card from './Components/Card/Card';
+import { useEffect } from "react";
+import "./App.css";
+import Card from "./Components/Card/Card";
+import { pokemonServices } from "./Services/Pokemon/Pokemon.services";
 
 function App() {
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const filters = { limit: 5, offset: 0 };
+        const response = await pokemonServices.getPokemons(filters);
+
+        console.log({ response });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchPokemons();
+  }, []);
   return (
     <div className="w-full min-h-fullscreen flex flex-col bg-gray-200">
-      <header className="w-full h-16 bg-white sticky top-0 left-0">
-
-      </header>
+      <header className="w-full h-16 bg-white sticky top-0 left-0"></header>
 
       <Card></Card>
     </div>
